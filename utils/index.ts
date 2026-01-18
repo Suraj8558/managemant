3507e733a58f9b363256e5dced4b8cde91c268fb
suraj <surajsk8558@gmail.com>
2026-01-18 15:19:55 +0530

    feat: Add utility functions for calculating natural numbers and enhance factorial operations

--- utils/index.ts
+++ utils/index.ts
@@ -6,13 +6,35 @@ import {
 } from "./PrintNumber.ts";
 
 import { EvenNumber , PrintEvenNumber} from "./EvenNumbers.ts";
+import { PrintOddNumber, PrintOddNumberSimple } from "./OddNumbers.ts"
+import { GetNaturalNumber, getNaturalNumberSum } from "./GetNaturalNumber.ts";
+import { factorailWhile, Factorial, factorialResursive } from "./Factorial.ts";
+GetNaturalNumber
+
 // Basic JavaScript Logic
 // 1. Print numbers 1 to 100
 // PrintNumberWithForLoop(1, 100);
 // PrintNumberWithForWhileLoop(1, 100);
-// PrintNumberWithForDoWhileLoop(1, 100);
+// PrintNumberWithForDoWhileLoop(1, 100);Calculate sum of first 10 natural numbers
 // PrintNumWithRecursion(1, 100);
 
 // Print even numbers 1 to 50
 // EvenNumber(1, 50)
 // PrintEvenNumber(13)
+
+
+// Print odd numbers 1 to 50
+// console.log(PrintOddNumber(50))
+// const isOdd =  PrintOddNumberSimple(10)
+// console.log("isOdd", isOdd)
+
+// Calculate sum of first 10 natural numbers
+// console.log("natural numbers :-", GetNaturalNumber(10))
+// console.log("natural numbers :-", getNaturalNumberSum(10))
+
+
+
+// console.log("Factorial Number:-", Factorial(4))
+// console.log("Factorial Number:-", factorialResursive(4))
+// console.log("Factorial Number:-", factorailWhile(20))
+// console.log("Factorial Number:-", factorailWhile(50).toString())
