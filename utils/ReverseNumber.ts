eeedbe9ce6aa8e92ca4113c03c481b6b97bcb8a8
suraj <surajsk8558@gmail.com>
2026-01-20 00:12:10 +0530

    feat: Implement additional utility functions for reversing numbers

--- utils/ReverseNumber.ts
+++ utils/ReverseNumber.ts
@@ -1,5 +1,25 @@
-export const ResverseNumber = (num: number):number => {
-  let lastdigit = num % 10;
-   let shift =  lastdigit * 10  
-} 
+export const reverseNumberFor = (num: number): number => {
+  let reversed = 0;
+  let n = Math.abs(num);
 
+  for (; n > 0; n = Math.floor(n / 10)) {
+    reversed = reversed * 10 + (n % 10);
+  }
+
+  return num < 0 ? -reversed : reversed;
+}
+
+
+export const reverseNumberString = (num: number): number => {
+  const reversed = Math.abs(num)
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  return num < 0 ? -Number(reversed) : Number(reversed);
+}
+
+
+console.log(reverseNumberString(12345678))
+console.log(reverseNumberFor(123456))
